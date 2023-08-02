d81337b16c230e2cfda5040f9b232a794fc7dab2
hamidr-source <hr454837@gmail.com>
2023-08-02 02:23:19 -0700

    Add bmi function

--- Go/web_server/server/handlers.go
+++ Go/web_server/server/handlers.go
@@ -1,10 +1,40 @@
 package server
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 )
 
 func (handler *server) Hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello from hamid!")
+}
+
+func (handler *server) Bmi(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		fmt.Fprintf(w, "sorry, only post method can given!")
+	} else {
+		defer r.Body.Close()
+
+		rawBody , err := io.ReadAll(r.Body)
+		if err != nil {
+			panic(err)
+		}		
+
+		body := &struct{
+			height float64 `json:"height"`
+			weight float64 `json:"weight"`
+		}{}
+
+		err = json.Unmarshal(rawBody, body)
+		if err != nil {
+			panic(err)
+		}	
+
+		if body.height < 40 || body.height > 250 {
+			fmt.Fprintf(w, "invalid height provided %.2f", body.height)
+			return
+		}
+	}
 }
