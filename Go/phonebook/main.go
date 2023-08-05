5f7d5ab9cbb6a1d3fd5b35fa93251bdae169412b
hamidr-source <hr454837@gmail.com>
2023-08-05 05:39:33 -0700

    Create add command for root

--- Go/phonebook/main.go
+++ Go/phonebook/main.go
@@ -1,8 +1,26 @@
 package main
 
-import "github.com/spf13/cobra"
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/spf13/cobra"
+)
 
 func main() {
 	const discription = "Phonebook Application"
 	root := &cobra.Command{Short: discription}
+
+	trap := make(chan os.Signal, 1)
+	signal.Notify(trap, syscall.SIGINT, syscall.SIGTERM)
+
+	root.AddCommand(
+		//
+	)
+
+	if err := root.Execute(); err != nil {
+		log.Fatal("faield to excute root command: \n%v", err)
+	}
 }
