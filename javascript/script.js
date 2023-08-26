a5bdbc8fd63248df89c233a33b75244a7664af97
hamidr-source <hr454837@gmail.com>
2023-08-26 03:42:54 -0700

    online shop practice

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,40 @@
-let userFirstNumber = Number(prompt("Please enter a number")) 
-let userSecondNumber = Number(prompt("Please enter a number"))
-let sum = 1
-for (let index = 0; index < userSecondNumber; index++) {
-    sum *= userFirstNumber
+const product = [
+  { id: 1, name: "laptop", price: 1000 },
+  { id: 2, name: "Mobile", price: 400 },
+  { id: 3, name: "Tablet", price: 800 },
+  { id: 4, name: "Pen", price: 2 },
+  { id: 5, name: "Pencil", price: 1.5 },
+  { id: 5, name: "Phone", price: 300 },
+];
+
+const userBasket = [
+  { id: 1, name: "Pen", price: 2 },
+  { id: 2, name: "Pencil", price: 1.5 },
+];
+
+let productName = String(prompt("Please enter name of your product. "));
+let requestProduct;
+
+let isValid = product.some(function (product) {
+  if (product.name === productName) {
+    requestProduct = product;
+    return true;
+  }
+});
+
+if (isValid === true) {
+  let newProduct = {
+    id: 3,
+    name: requestProduct.name,
+    price: requestProduct.price,
+  };
+  userBasket.push(newProduct);
+  let sum = 0;
+  userBasket.forEach(function (product) {
+    console.log(product);
+    sum = sum + product.price;
+    console.log(sum);
+  });
+} else {
+  alert("Is not valid");
 }
-console.log(sum)
