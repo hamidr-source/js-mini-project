b9e5cc8d646803c645af29bef15e3804b3904096
hamidr-source <hr454837@gmail.com>
2023-08-25 23:51:44 -0700

    Previous practice with while loop

--- javascript/script.js
+++ javascript/script.js
@@ -1,12 +1,15 @@
 let userFirstNumber = Number(prompt("enter your first number"))
 let userSecondNumber = Number(prompt("enter your second number"))
-let counter = 0
-for (let index = userFirstNumber; index < userSecondNumber; index++) {
-    counter = index++
-    if (counter % 2 === 0){
-        console.log(counter)
-    }else if(counter % 2 !== 0){
-        counter++ 
-        console.log(counter)
+
+if (userFirstNumber % 2  === 0 ) {
+    while(userFirstNumber < userSecondNumber){
+        console.log(userFirstNumber)
+        userFirstNumber += 2
+    }
+} else {
+    userFirstNumber++
+    while(userFirstNumber < userSecondNumber){
+        console.log(userFirstNumber)
+        userFirstNumber += 2
     }
 }
