104995eff4225aa59e8a82300663b482690a3de7
hamidr-source <hr454837@gmail.com>
2023-08-26 05:17:30 -0700

    Post cost calculator program

--- javascript/script.js
+++ javascript/script.js
@@ -1,20 +1,34 @@
-const users = [
-  { id: 1, name: "Ali", age: 23 },
-  { id: 2, name: "Hamid", age: 30 },
-  { id: 3, name: "Amir", age: 22 },
-  { id: 4, name: "Akbar", age: 18 },
-  { id: 5, name: "Soheil", age: 20 },
-  { id: 5, name: "Reza", age: 20 }
+let sum = 0
+
+// User basket shop
+const userBasket = [
+  { id: 1, name: "Phone", price: 90 },
+  { id: 2, name: "Laptop", price: 1000 },
+  { id: 3, name: "Pen", price: 90 },
+  { id: 4, name: "Pencil", price: 90 },
+  { id: 5, name: "Jacket", price: 120 },
+  { id: 5, name: "Milk", price: 90 },
 ];
 
-let result = users.every(function ( user ) {
-    console.log(user)
-    return user.age >= 18
-    
-})
+// find products price grater than 100
+let filterProducts = userBasket.filter(function (product) {
+  return product.price < 100;
+});
 
-if (result === true) {
-    console.log("success")
-} else {
-    console.log("not success")
-}
+// Calc the price of post
+let postProducts = filterProducts.length * 10;
+
+
+// Test postProducts variable
+console.log(postProducts)
+
+// All prices
+userBasket.map(function (product) {
+  let allPrice = postProducts + product.price;
+  sum = sum + allPrice
+});
+
+// Result
+console.log(sum);
+
+
