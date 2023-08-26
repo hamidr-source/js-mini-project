081f16763a267c225318720b07d877e541fb82ba
hamidr-source <hr454837@gmail.com>
2023-08-26 00:17:25 -0700

    Algorithm practice

--- javascript/script.js
+++ javascript/script.js
@@ -1,15 +1,7 @@
-let userFirstNumber = Number(prompt("enter your first number"))
-let userSecondNumber = Number(prompt("enter your second number"))
-
-if (userFirstNumber % 2  === 0 ) {
-    while(userFirstNumber < userSecondNumber){
-        console.log(userFirstNumber)
-        userFirstNumber += 2
-    }
-} else {
-    userFirstNumber++
-    while(userFirstNumber < userSecondNumber){
-        console.log(userFirstNumber)
-        userFirstNumber += 2
-    }
+let userFirstNumber = Number(prompt("Please enter a number")) 
+let userSecondNumber = Number(prompt("Please enter a number"))
+let sum = 1
+for (let index = 0; index < userSecondNumber; index++) {
+    sum *= userFirstNumber
 }
+console.log(sum)
