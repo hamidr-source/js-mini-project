2c676f1dadc2a09442275d649033b8e09c8a3d64
hamidr-source <hr454837@gmail.com>
2023-08-26 11:27:20 -0700

    Reverse word program

--- javascript/script.js
+++ javascript/script.js
@@ -1,34 +1,10 @@
-let sum = 0
+let userEntryWord = String(prompt("Please enter a word", ""))
+let wordArray = userEntryWord.split("")
 
-// User basket shop
-const userBasket = [
-  { id: 1, name: "Phone", price: 90 },
-  { id: 2, name: "Laptop", price: 1000 },
-  { id: 3, name: "Pen", price: 90 },
-  { id: 4, name: "Pencil", price: 90 },
-  { id: 5, name: "Jacket", price: 120 },
-  { id: 5, name: "Milk", price: 90 },
-];
+let reverseWord = wordArray.reverse()
 
-// find products price grater than 100
-let filterProducts = userBasket.filter(function (product) {
-  return product.price < 100;
-});
-
-// Calc the price of post
-let postProducts = filterProducts.length * 10;
-
-
-// Test postProducts variable
-console.log(postProducts)
-
-// All prices
-userBasket.map(function (product) {
-  let allPrice = postProducts + product.price;
-  sum = sum + allPrice
-});
-
-// Result
-console.log(sum);
-
-
+if (reverseWord === wordArray) {
+    alert("it's ok")
+}else {
+    alert("it's not ok")
+}
