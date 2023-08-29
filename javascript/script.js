94a670e8cac00b1e52e0c6be24f60bdf8c469b6b
hamidr-source <hr454837@gmail.com>
2023-08-28 23:38:50 -0700

    Add style and database

--- javascript/script.js
+++ javascript/script.js
@@ -1,22 +1,16 @@
-let selectCountry = document.querySelector('.countrySelect')
-let selectCity = document.querySelector('.citySelect')
-const cities = {
-    Iran: ["tehran", "tabriz", "mashhad", "isfahan", "shiraz"],
-    USA: ["los angles", "new york", "washington", "texas"],
-    Canada: ["vancouver", "quebec", "toronto"]
-}
+const citiesData = [
+  { city: "Tehran", temp: 12, weather: "sunny", humidity: 23, windSpeed: 32 },
+  { city: "Tabriz", temp: 5, weather: "windy", humidity: 10, windSpeed: 40 },
+  { city: "Amol", temp: 6, weather: "rainy", humidity: 29, windSpeed: 20 },
+  { city: "Isfahan", temp: 10, weather: "sunny", humidity: 25, windSpeed: 32 },
+  { city: "Mashhad", temp: -3, weather: "snowy", humidity: 20, windSpeed: 30 },
+];
 
-
-selectCountry.addEventListener('change', function () {
-    if (selectCountry.value === 'Please Select') {
-        selectCity.innerHTML = ''
-        selectCity.innerHTML += '<option>Select Country</option>'
-    }else {
-        let mainCountry = selectCountry.value
-        let mainCities = cities[mainCountry]
-        selectCity.innerHTML = ''
-        mainCities.forEach(function (city) {
-        selectCity.innerHTML += '<option>' + city + '</option>'
-        });
-    }
-})
+const images = [
+    "./img/'Porsche 911 Fuji Retro' Poster by Pixaverse _ Displate.jpeg",
+    "./img/7628941.jpg",
+    "./img/[2560x1600] Great Wave Off Kanagawa (re-upload).jpeg",
+    "./img/sky.jpg",
+    "./img/Levi Ackerman wallpaper by _natsuke - Download on ZEDGE™ _ 0fbc.jpeg",
+    "./img/download (2).jpeg"
+]
