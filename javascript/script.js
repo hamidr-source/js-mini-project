20bb5ff3008bfe1f1b2d52abddfb9cc042974968
hamidr-source <hr454837@gmail.com>
2023-09-05 06:37:33 -0700

    Refactor addTodo function

--- javascript/script.js
+++ javascript/script.js
@@ -1,34 +1,37 @@
-const todoInput = document.querySelector(".todo-input");
-const todoBtn = document.querySelector(".todo-btn");
-const todoList = document.querySelector(".todo-list");
-const alertDiv = document.querySelector(".alert-div");
+const $ = document
+const todoInput = $.querySelector(".todo-input");
+const todoBtn = $.querySelector(".todo-btn");
+const todoList = $.querySelector(".todo-list");
+const alertDiv = $.querySelector(".alert-div");
+const todoDiv = $.querySelector('.hidden-todo')
+const completeBtn = $.querySelector('.completed-btn')
+const trashBtn = $.querySelector('.trash-btn')
+
 todoBtn.addEventListener("click", addTodo);
+
 
 function addTodo(event) {
   event.preventDefault();
 
   if (todoInput.value !== "") {
     const inputValue = todoInput.value;
-    const todoDiv = document.createElement("div");
-    const liItems = document.createElement("li");
-    const completeBtn = document.createElement("button");
-    const trashBtn = document.createElement("button");
+
+    const liItems = $.createElement("li");
 
     todoList.appendChild(todoDiv);
+    todoDiv.classList.remove("hidden-todo")
     todoDiv.classList.add("todo");
-    // console.log(inputValue)
+
     todoDiv.appendChild(liItems);
     liItems.innerHTML = inputValue;
     liItems.classList.add("todo-item");
-    completeBtn.innerHTML = '<i class="fas fa-check"></i>';
-    completeBtn.classList.add("completed-btn");
     todoDiv.appendChild(completeBtn);
-    trashBtn.innerHTML = '<i class="fas fa-trash"></i>';
-    trashBtn.classList.add("trash-btn");
     todoDiv.appendChild(trashBtn);
+
     alertDiv.style.display = "inline";
     alertDiv.innerHTML = "Task added";
     alertDiv.style.background = "rgb(56, 156, 56)";
+
     todoInput.value = "";
   } else {
     alertDiv.style.display = "inline";
