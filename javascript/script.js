cdee9d291b3a358d95a9359e170c99a505ae65ba
hamidr-source <hr454837@gmail.com>
2023-09-06 00:25:27 -0700

    Add context menu mini project

--- javascript/script.js
+++ javascript/script.js
@@ -1,59 +1,25 @@
-const $ = document;
-const todoInput = $.querySelector(".todo-input");
-const todoBtn = $.querySelector(".todo-btn");
-const todoList = $.querySelector(".todo-list");
-const alertDiv = $.querySelector(".alert-div");
+const contextMenu = document.querySelector(".context-menu");
 
-todoBtn.addEventListener("click", addTodo);
-todoList.addEventListener("click", deleteCheckTodo);
+document.body.addEventListener("contextmenu", contextHandler);
+document.body.addEventListener("click", clickHandler);
+document.body.addEventListener("keydown", keyHandler);
 
-function addTodo(event) {
+function contextHandler(event) {
   event.preventDefault();
 
-  const todoValue = todoInput.value;
-  const todoDiv = $.createElement("div");
-  const liItem = $.createElement("li");
-  const completeBtn = $.createElement("button");
-  completeBtn.innerHTML = '<i class="fas fa-check"></i>';
-  completeBtn.classList.add("completed-btn");
-  const trashBtn = $.createElement("button");
-  trashBtn.innerHTML = '<i class="fas fa-trash"></i>';
-  trashBtn.classList.add("trash-btn");
-
-  if (todoValue !== "") {
-    console.log(todoValue);
-    alertDiv.style.display = "inline";
-    alertDiv.innerHTML = "Task Added success !";
-    alertDiv.style.backgroundColor = "rgba(0, 136, 0, 0.411)";
-
-    todoList.appendChild(todoDiv);
-    todoDiv.classList.add("todo");
-    todoDiv.appendChild(liItem);
-    liItem.innerHTML = todoValue;
-    liItem.classList.add("todo-item");
-    todoDiv.appendChild(completeBtn);
-    todoDiv.appendChild(trashBtn);
-
-    todoInput.value = "";
-  } else {
-    alertDiv.style.display = "inline";
-    alertDiv.innerHTML = "Please enter a task !";
-    alertDiv.style.backgroundColor = "rgb(223, 28, 28)";
+  if ((contextMenu.style.display = "none")) {
+    contextMenu.style.display = "block";
+    contextMenu.style.left = event.pageX + "px";
+    contextMenu.style.top = event.pageY + "px";
   }
-
-  setTimeout(function () {
-    alertDiv.style.display = "none";
-  }, 3000);
 }
 
-function removeTodo(event) {
-  console.log(event.target);
+function clickHandler() {
+  contextMenu.style.display = "none";
 }
 
-function deleteCheckTodo(event) {
-  if (event.target.classList.value === "trash-btn") {
-    event.target.parentElement.remove();
-  } else if (event.target.classList.value === "completed-btn") {
-    event.target.parentElement.classList.toggle("completed");
+function keyHandler(event) {
+  if (event.key === "Escape") {
+    contextMenu.style.display = "none";
   }
 }
