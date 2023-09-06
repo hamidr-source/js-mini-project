2273b79f8a54ea0f3a82d052cdd96fcdf240701e
hamidr-source <hr454837@gmail.com>
2023-09-06 05:08:32 -0700

    Refactor timer mini project

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,25 @@
-const loader = document.querySelector(".loader");
+let minuteUserEntry = Number(prompt("Minute"));
+let secondUserEntry = Number(prompt("second"));
 
-window.addEventListener("load", loadHandler);
-function loadHandler() {
-  loader.classList.add("hidden");
-}
+let result = setInterval(function () {
+
+  if (secondUserEntry == -1){
+    minuteUserEntry--
+    secondUserEntry = 59
+  }
+
+  if (minuteUserEntry < 10 && minuteUserEntry[0] !== "0") {
+    minuteUserEntry = "0" + minuteUserEntry;
+  }
+
+  if (secondUserEntry < 10 && secondUserEntry[0] !== "0") {
+    secondUserEntry = "0" + secondUserEntry;
+  }
+
+  if (secondUserEntry == 0 && minuteUserEntry == 0){
+    clearInterval(result)
+  }
+  let timer = minuteUserEntry + ":" + secondUserEntry;
+  secondUserEntry--;
+  console.log(timer);
+}, 1000);
