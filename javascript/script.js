4e3ceb8ad44e6a7fb3ec78084087e0a16a6f307b
hamidr-source <hr454837@gmail.com>
2023-09-06 01:48:09 -0700

    Add  preloader mini project

--- javascript/script.js
+++ javascript/script.js
@@ -1,25 +1,6 @@
-const contextMenu = document.querySelector(".context-menu");
+const loader = document.querySelector(".loader");
 
-document.body.addEventListener("contextmenu", contextHandler);
-document.body.addEventListener("click", clickHandler);
-document.body.addEventListener("keydown", keyHandler);
-
-function contextHandler(event) {
-  event.preventDefault();
-
-  if ((contextMenu.style.display = "none")) {
-    contextMenu.style.display = "block";
-    contextMenu.style.left = event.pageX + "px";
-    contextMenu.style.top = event.pageY + "px";
-  }
-}
-
-function clickHandler() {
-  contextMenu.style.display = "none";
-}
-
-function keyHandler(event) {
-  if (event.key === "Escape") {
-    contextMenu.style.display = "none";
-  }
+window.addEventListener("load", loadHandler);
+function loadHandler() {
+  loader.classList.add("hidden");
 }
