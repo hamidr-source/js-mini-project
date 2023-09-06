7e62c190bbb920544643fe2141b1ea382eb34c11
hamidr-source <hr454837@gmail.com>
2023-09-06 08:59:25 -0700

    Add max length practice

--- javascript/script.js
+++ javascript/script.js
@@ -1,25 +1,8 @@
-let minuteUserEntry = Number(prompt("Minute"));
-let secondUserEntry = Number(prompt("second"));
+const usernameInput = document.querySelector(".username");
+let counter = document.querySelector(".counter");
 
-let result = setInterval(function () {
+usernameInput.addEventListener("keydown", keyDownHandler);
 
-  if (secondUserEntry == -1){
-    minuteUserEntry--
-    secondUserEntry = 59
-  }
-
-  if (minuteUserEntry < 10 && minuteUserEntry[0] !== "0") {
-    minuteUserEntry = "0" + minuteUserEntry;
-  }
-
-  if (secondUserEntry < 10 && secondUserEntry[0] !== "0") {
-    secondUserEntry = "0" + secondUserEntry;
-  }
-
-  if (secondUserEntry == 0 && minuteUserEntry == 0){
-    clearInterval(result)
-  }
-  let timer = minuteUserEntry + ":" + secondUserEntry;
-  secondUserEntry--;
-  console.log(timer);
-}, 1000);
+function keyDownHandler() {
+  counter.innerHTML = 19 - usernameInput.value.length;
+}
