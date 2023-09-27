f09f601f17277f1d917b1ead512885dc0c954298
hamidr-source <hr454837@gmail.com>
2023-09-27 02:31:38 -0700

    Add next and previous music function

--- javascript/script.js
+++ javascript/script.js
@@ -4,59 +4,91 @@ const mediaCover = $.querySelector("#cover");
 const mediaTitle = $.querySelector("#title");
 const mediaArtist = $.querySelector("#artist");
 const mediaAudio = $.querySelector("audio");
-const playBtn = $.querySelector("#play")
+const playBtn = $.querySelector("#play");
+const nextBtn = $.querySelector("#next");
+const prevBtn = $.querySelector("#prev");
 
 const musicData = [
   {
     path: "./Media/Erfan Tahmasbi - To.mp3",
     displayName: "To",
     artist: "Erfan Tahmasbi",
-    cover:
-      "./Images/Erfan-Tahmasbi-To.jpg",
+    cover: "./Images/Erfan-Tahmasbi-To.jpg",
   },
   {
     path: "./Media/Kimia - Hame On Rooza 128.mp3",
     displayName: "Hame On Rooza",
     artist: "Kimia",
-    cover:
-      "./Images/Hame-On-Roza.jpg",
+    cover: "./Images/Hame-On-Roza.jpg",
   },
   {
     path: "./Media/Hamid Hiraad - Zibaye Man.mp3",
     displayName: "Zibaye Man",
     artist: "Hamid Hiraad",
-    cover:
-      "./Images/Zibay-Man.jpg",
+    cover: "./Images/Zibay-Man.jpg",
   },
 ];
+let musicIndex = 1;
+let isPlaying = false;
 
-let isPlaying = false 
-
-function playMusic () {
-  isPlaying = true
-  playBtn.classList.replace("fa-play", "fa-pause")
-  playBtn.setAttribute("title", "pause")
-  mediaAudio.play()
-}
- 
-
-function songLoad (musicData) {
-  mediaTitle.textContent = musicData.displayName
-  mediaArtist.textContent = musicData.artist
-  mediaAudio.src = musicData.path
-  changeCover(musicData.cover)
-  console.log(mediaAudio.src)
+function playMusic() {
+  isPlaying = true;
+  playBtn.classList.replace("fa-play", "fa-pause");
+  playBtn.setAttribute("title", "pause");
+  mediaAudio.play();
 }
 
-function changeCover (cover) {
+function pauseMusic() {
+  isPlaying = false;
+  playBtn.classList.replace("fa-pause", "fa-play");
+  playBtn.setAttribute("title", "play");
+  mediaAudio.pause();
+}
 
-  mediaCover.classList.remove("active")
+function songLoad(musicData) {
+  mediaTitle.textContent = musicData.displayName;
+  mediaArtist.textContent = musicData.artist;
+  mediaAudio.src = musicData.path;
+  changeCover(musicData.cover);
+  console.log(mediaAudio.src);
+}
+
+function changeCover(cover) {
+  mediaCover.classList.remove("active");
   setTimeout(function () {
-    mediaCover.src = cover
-    mediaCover.classList.add("active")
-  }, 100)
-  bgContainer.src = cover
+    mediaCover.src = cover;
+    mediaCover.classList.add("active");
+  }, 100);
+  bgContainer.src = cover;
 }
-songLoad(musicData[0])
+songLoad(musicData[musicIndex]);
 
-playBtn.addEventListener("click", playMusic)
+function nextMusic() {
+  musicIndex++;
+  console.log(mediaAudio.src);
+  if (musicIndex > musicData.length - 1) {
+    musicIndex = 0;
+  }
+  songLoad(musicData[musicIndex]);
+  playMusic()
+}
+
+function prevMusic () {
+  musicIndex--
+  console.log(mediaAudio.src)
+  if (musicIndex < 0) {
+    musicIndex = musicData.length - 1
+  }
+  songLoad(musicData[musicIndex]);
+  playMusic()
+}
+
+playBtn.addEventListener("click", function () {
+  if (isPlaying) {
+    pauseMusic();
+  } else {
+    playMusic();
+  }
+});
+nextBtn.addEventListener("click", nextMusic);
+prevBtn.addEventListener("click", prevMusic);
