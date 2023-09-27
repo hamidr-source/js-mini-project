0060f010183ef5743fff0374a358e7e58d216959
hamidr-source <hr454837@gmail.com>
2023-09-27 04:01:55 -0700

    Add time duration for musics

--- javascript/script.js
+++ javascript/script.js
@@ -7,6 +7,10 @@ const mediaAudio = $.querySelector("audio");
 const playBtn = $.querySelector("#play");
 const nextBtn = $.querySelector("#next");
 const prevBtn = $.querySelector("#prev");
+const progressContainer = $.querySelector("#progress-container");
+const progress = $.querySelector("#progress");
+const currentTimeElem = $.querySelector("#current-time");
+const durationElem = $.querySelector("#duration");
 
 const musicData = [
   {
@@ -70,17 +74,46 @@ function nextMusic() {
     musicIndex = 0;
   }
   songLoad(musicData[musicIndex]);
-  playMusic()
+  playMusic();
 }
 
-function prevMusic () {
-  musicIndex--
-  console.log(mediaAudio.src)
+function prevMusic() {
+  musicIndex--;
+  console.log(mediaAudio.src);
   if (musicIndex < 0) {
-    musicIndex = musicData.length - 1
+    musicIndex = musicData.length - 1;
   }
   songLoad(musicData[musicIndex]);
-  playMusic()
+  playMusic();
+}
+
+function updateProgressBar (event) {
+  const duration = event.srcElement.duration
+  const currentTime = event.srcElement.currentTime
+  const progressPercent = (currentTime / duration) * 100
+  progress.style.width = progressPercent + "%";
+  const durationMinutes = Math.floor(duration / 60)
+  let durationSeconds = Math.floor(duration % 60)
+  if (durationSeconds < 10) {
+    durationSeconds = "0" + durationSeconds;
+  }
+  if (durationSeconds) {
+    durationElem.textContent = durationMinutes + ":" + durationSeconds;
+  }
+  const currentMinutes = Math.floor(currentTime / 60);
+  let currentSeconds = Math.floor(currentTime % 60);
+  if (currentSeconds < 10) {
+    currentSeconds = "0" + currentSeconds;
+  }
+  currentTimeElem.textContent = currentMinutes + ":" + currentSeconds;
+}
+
+function setProgressBar(event) {
+  const width = this.clientWidth
+  const clickX = event.offsetX
+  console.log(clickX)
+  const musicDuration = mediaAudio.duration
+  mediaAudio.currentTime = (clickX / width) * musicDuration
 }
 
 playBtn.addEventListener("click", function () {
@@ -92,3 +125,6 @@ playBtn.addEventListener("click", function () {
 });
 nextBtn.addEventListener("click", nextMusic);
 prevBtn.addEventListener("click", prevMusic);
+mediaAudio.addEventListener("ended", nextMusic);
+mediaAudio.addEventListener("timeupdate", updateProgressBar)
+progressContainer.addEventListener("click", setProgressBar)
