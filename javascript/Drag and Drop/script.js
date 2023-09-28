de4f9791ec20f5c5d4c2b2f033a54cdcc5af3513
hamidr-source <hr454837@gmail.com>
2023-09-28 14:24:45 -0700

    Add drag and drop practice

---
+++ javascript/Drag and Drop/script.js
@@ -0,0 +1,18 @@
+function dragStartHandler (event) {
+    console.log(event.target.id)
+    event.dataTransfer.setData("elemId", event.target.id)
+}
+
+function dropHandler (event) {
+    console.log(event)
+    let targetId = event.dataTransfer.getData("elemId")
+    let targetElem = document.getElementById(targetId)
+    event.target.append(targetElem)
+}
+
+function dragOverHandler (event) {
+    event.preventDefault()
+}
+
+// drag -> ondragstart, ondrag, ondragend
+// drop ->  
