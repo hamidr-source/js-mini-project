ad360651f4f3c84945ad6b299eef762b5c4520ba
hamidr-source <hr454837@gmail.com>
2023-09-29 01:20:22 -0700

    Refactor dark/light mode

--- javascript/Dark and Light Mode/app.js
+++ javascript/Dark and Light Mode/app.js
@@ -3,12 +3,11 @@ const switchBtn = $.querySelector(".switch")
 let isDark = false
 
 switchBtn.addEventListener("click", function () {
+    $.body.classList.toggle("dark")
     if (!isDark) {
-        $.body.classList.add("dark")
         localStorage.setItem("theme", "dark")
         isDark = true
     } else {
-        $.body.classList.remove("dark")
         localStorage.setItem("theme", "light")
         isDark = false
     }
