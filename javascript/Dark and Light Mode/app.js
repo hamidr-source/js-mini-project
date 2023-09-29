7e129cda00f11b6e024fcf40ab0f68a84b762813
hamidr-source <hr454837@gmail.com>
2023-09-29 01:14:26 -0700

    Refactor light/dark mode

--- javascript/Dark and Light Mode/app.js
+++ javascript/Dark and Light Mode/app.js
@@ -1,20 +1,23 @@
 const $ = document
 const switchBtn = $.querySelector(".switch")
+let isDark = false
+
+switchBtn.addEventListener("click", function () {
+    if (!isDark) {
+        $.body.classList.add("dark")
+        localStorage.setItem("theme", "dark")
+        isDark = true
+    } else {
+        $.body.classList.remove("dark")
+        localStorage.setItem("theme", "light")
+        isDark = false
+    }
+})
 
 window.addEventListener("load", function () {
     let localStorageThemeValue = localStorage.getItem("theme")
     if (localStorageThemeValue === "dark") {
         $.body.classList.add("dark")
-    }
-})
-
-switchBtn.addEventListener("click", function () {
-    if ($.body.classList.value === "") {
-        $.body.classList.add("dark")
-        localStorage.setItem("theme", "dark")
-    } else {
-        $.body.classList.remove("dark")
-        localStorage.setItem("theme", "light")
     }
 })
 
