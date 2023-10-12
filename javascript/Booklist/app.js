1ff40214f819d3f03aedc8f557c536961e24f2ac
hamidr-source <hr454837@gmail.com>
2023-10-12 11:32:16 -0700

    Add book generator

--- javascript/Booklist/app.js
+++ javascript/Booklist/app.js
@@ -3,20 +3,38 @@ const bookTitle = $.querySelector("#title");
 const bookAuthor = $.querySelector("#author");
 const bookYear = $.querySelector("#year");
 const btn = $.querySelector(".btn");
+const bookList = $.querySelector("#book-list")
 let allBooks = [];
 
-function addBook() {
+function addBook(event) {
+  event.preventDefault();
   let book = {
-    id:     allBooks.length + 1,
-    title:  bookTitle.value,
+    id: allBooks.length + 1,
+    title: bookTitle.value,
     author: bookAuthor.value,
-    year:   bookYear.value,
+    year: bookYear.value,
   };
-  allBooks.push(book)
+  allBooks.push(book);
   if (bookTitle.value !== "") {
-    bookGenerator(allBooks)
-    getLocalStorage(allBooks)
-  } 
+    bookGenerator(allBooks);
+    getLocalStorage(allBooks);
+  }
+}
+
+function bookGenerator(books) {
+  let tableRow, title, author, year;
+  bookList.innerHTML = ""
+  books.forEach(function (book) {
+    tableRow = $.createElement("tr")
+    title = $.createElement("td")
+    author = $.createElement("td")
+    year = $.createElement("td")
+    title.innerHTML = book.title
+    author.innerHTML = book.author
+    year.innerHTML = book.year
+    tableRow.append(title,author,year)
+    bookList.appendChild(tableRow) 
+  });
 }
 
 btn.addEventListener("click", addBook);
