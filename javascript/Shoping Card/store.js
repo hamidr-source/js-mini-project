99ebdd376e276e3b27f98eaf3ae501361d432c92
hamidr-source <hr454837@gmail.com>
2023-10-17 07:19:14 -0700

    Add styles and products data

---
+++ javascript/Shoping Card/store.js
@@ -0,0 +1,10 @@
+const productsData = [
+    { id: 1, title: 'Album 1', price: 12.93, img: 'Images/Album 1.png' },
+    { id: 2, title: 'Album 2', price: 21, img: 'Images/Album 2.png' },
+    { id: 3, title: 'Album 3', price: 33, img: 'Images/Album 3.png' },
+    { id: 4, title: 'Album 4', price: 41.98, img: 'Images/Album 4.png' },
+    { id: 5, title: 'Coffee', price: 98, img: 'Images/Cofee.png' },
+    { id: 6, title: 'Shirt', price: 65.33, img: 'Images/Shirt.png' },
+]
+
+
