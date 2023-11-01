5c9787bb51a656baf4d5de8091d5acb2e24f394a
hamidr-source <hr454837@gmail.com>
2023-11-01 01:11:30 -0700

    Add main products for get the product id

--- javascript/Dynamic Shoes Store/src/product.js
+++ javascript/Dynamic Shoes Store/src/product.js
@@ -1,5 +1,22 @@
-const backBtn = document.querySelector(".back-btn")
+const backBtn = document.querySelector(".back-btn");
+const shoesTitle = document.querySelector(".shoes-title");
+const shoesDesc = document.querySelector(".shoes-description");
+const shoesImage = document.querySelector("img");
+
+const productsData = [
+  { id: 1, title: "Sport Shoes", price: 35, img: "images/1.png" },
+  { id: 2, title: "Women Shoes", price: 55, img: "images/2.png" },
+  { id: 3, title: "Men Shoes", price: 22, img: "images/3.png" },
+];
+
+const locationParams = new URLSearchParams(location.search);
+const productID = locationParams.get("id");
+
+let mainProduct = productsData.find(function (product) {
+  return product.id === Number(productID);
+});
+
 
 backBtn.addEventListener("click", function () {
-    history.back()
-})
+  history.back();
+});
