8c354d6b28d09f37b7966c6e71377f3f46da0cfe
hamidr-source <hr454837@gmail.com>
2023-12-12 09:49:32 -0700

    Create the stores

--- javascript/Login Page/src/app.js
+++ javascript/Login Page/src/app.js
@@ -1,10 +1,27 @@
-window.addEventListener("load", () => {
-  const DBOpenReq = indexedDB.open("Login Page", 1);
+let db = null
+let objectStore = null
 
-  DBOpenReq.addEventListener("error", (err) => {
+window.addEventListener("load", () => {
+  const DBOpenReq = indexedDB.open("Login Page", 11);
+
+  DBOpenReq.addEventListener("error", err => {
     console.warn("Error:", err);
   });
-  DBOpenReq.addEventListener("success", (event) =>{
+  DBOpenReq.addEventListener("success", event =>{
     console.log("Success:", event)
+  })
+  DBOpenReq.addEventListener("upgradeneeded", event => {
+    db = event.target.result
+    console.log("Upgrade:", event.target.result)
+    
+    if (!db.objectStoreNames.contains("users")) {
+        objectStore = db.createObjectStore("users")    
+    }
+    
+    if (db.objectStoreNames.contains("courses")) {
+        objectStore = db.deleteObjectStore("courses")    
+    }
+    
+    console.log(db.objectStoreNames)
   })
 });
