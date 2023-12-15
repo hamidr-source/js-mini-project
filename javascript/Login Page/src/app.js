9b297971b7d3f3b0c4e34b79922f0391f652412a
hamidr-source <hr454837@gmail.com>
2023-12-14 23:36:54 -0700

    Add user info in database

--- javascript/Login Page/src/app.js
+++ javascript/Login Page/src/app.js
@@ -1,38 +1,63 @@
-const registerForm = document.querySelector(".login-form")
-const userNameInput = document.querySelector(".username-input")
-const passwordInput = document.querySelector(".password-input")
-const emailInput = document.querySelector(".email-input")
+const registerForm = document.querySelector(".login-form");
+const userNameInput = document.querySelector(".username-input");
+const passwordInput = document.querySelector(".password-input");
+const emailInput = document.querySelector(".email-input");
 
-let db = null
-let objectStore = null
+let db = null;
+let objectStore = null;
 
 window.addEventListener("load", () => {
-  const DBOpenReq = indexedDB.open("Login Page", 11);
+  const DBOpenReq = indexedDB.open("Login Page", 14);
 
-  DBOpenReq.addEventListener("error", err => {
+  DBOpenReq.addEventListener("error", (err) => {
     console.warn("Error:", err);
   });
-  DBOpenReq.addEventListener("success", event =>{
-    console.log("Success:", event)
-  })
-  DBOpenReq.addEventListener("upgradeneeded", event => {
+
+  DBOpenReq.addEventListener("success", (event) => {
+    console.log("Success:", event);
     db = event.target.result
-    console.log("Upgrade:", event.target.result)
-    
+  });
+
+  DBOpenReq.addEventListener("upgradeneeded", (event) => {
+    db = event.target.result;
+    console.log("Upgrade:", event.target.result);
+
     if (!db.objectStoreNames.contains("users")) {
-        objectStore = db.createObjectStore("users")    
+      objectStore = db.createObjectStore("users", {
+        keyPath: "id"
+      });
     }
-  })
+  });
 });
 
-registerForm.addEventListener("submit", event => {
-  event.preventDefault()
+registerForm.addEventListener("submit", (event) => {
+  event.preventDefault();
 
   let newUserLogger = {
     id: Math.floor(Math.random() * 9999),
     name: userNameInput.value,
     password: passwordInput.value,
-    email: emailInput.value
-  }
-  console.log(newUserLogger)
-})
+    email: emailInput.value,
+  };
+  
+  let tx = db.transaction("users", "readwrite")
+
+  tx.addEventListener("error", err => {
+    console.warn("Tx Error:", err)
+  })
+
+  tx.addEventListener("success", event => {
+    console.log("Tx Success:", event)
+  })
+
+  let store = tx.objectStore("users")
+  let req = store.add(newUserLogger)
+
+  req.addEventListener("error", err => {
+    console.warn("Request Error:", err)
+  })
+
+  req.addEventListener("success", event => {
+    console.log("Request Success:", event)
+  })
+});
