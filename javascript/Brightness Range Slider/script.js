969ad052777dbdc3832a9223e0123aac3d787bdd
hamidr-source <hr454837@gmail.com>
2024-03-05 23:16:29 -0700

    Rename all projects

--- javascript/Brightness Range Slider/script.js
+++
@@ -1,6 +0,0 @@
-const inputRange = document.querySelector("#range")
-
-inputRange.addEventListener("change", function (event) {
-    console.log(event.target.value) 
-    document.body.style.filter = 'brightness('+ event.target.value + '%)'  
-})
--- javascript/Dynamic Change bgColor/script.js
+++
@@ -1,7 +0,0 @@
-const colorsBtn = document.querySelectorAll(".btn")
-
-colorsBtn.forEach(function (colorBtn) {
-    colorBtn.addEventListener("click" , function() {
-        document.documentElement.style.setProperty("--theme-color", colorBtn.dataset.color)
-    })
-})
