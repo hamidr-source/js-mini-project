723a10d54d9a8c7aab41f25f6075f718da4174fb
hamidr-source <hr454837@gmail.com>
2024-03-12 06:28:08 -0700

    Refactor page loading code

--- javascript/json-placeholder-user-API/script.js
+++ javascript/json-placeholder-user-API/script.js
@@ -3,6 +3,10 @@ const deleteModal = document.querySelector("#delete-modal");
 let userID = null;
 
 window.addEventListener("load", () => {
+  getUser();
+});
+
+const getUser = () => {
   fetch("https://jsonplaceholder.typicode.com/users")
     .then((res) => res.json())
     .then((data) => {
@@ -12,24 +16,24 @@ window.addEventListener("load", () => {
         userContainer.insertAdjacentHTML(
           "beforeend",
           `     
-          <div class="user">
-            <div class="user-profile-wrap">
-                <img class="user-profile" src="img/noimg.png" alt="default-image">
-                <div class="user-profile-description">
-                    <h1 class="user-profile-name">${user.name} - ${user.username}<span class="user-age">${user.website}</span> </h1>
-                    <h3 class="user-explanations">Email: ${user.email}</h3>
-                </div>
-            </div>
-            <div class="btn-groups-column">
-                <button class="delete-user-btn" onclick="openDeleteModal(${user.id})">delete</button>
-                <button class="edit-user-btn">edit</button>
-            </div>
-        </div>
-        `
+        <div class="user">
+          <div class="user-profile-wrap">
+              <img class="user-profile" src="img/noimg.png" alt="default-image">
+              <div class="user-profile-description">
+                  <h1 class="user-profile-name">${user.name} - ${user.username}<span class="user-age">${user.website}</span> </h1>
+                  <h3 class="user-explanations">Email: ${user.email}</h3>
+              </div>
+          </div>
+          <div class="btn-groups-column">
+              <button class="delete-user-btn" onclick="openDeleteModal(${user.id})">delete</button>
+              <button class="edit-user-btn">edit</button>
+          </div>
+      </div>
+      `
         );
       });
     });
-});
+};
 
 const openDeleteModal = (id) => {
   userID = id;
