2b107fdd6ed171f2e25a85eaf924b2fc522b6dbb
hamidr-source <hr454837@gmail.com>
2024-03-12 01:40:24 -0700

    Add get users from ApI

--- javascript/json-placeholder-user-API/script.js
+++ javascript/json-placeholder-user-API/script.js
@@ -1,4 +1,32 @@
+const userContainer = document.querySelector("#wrap-users");
+
 window.addEventListener("load", () => {
   fetch("https://jsonplaceholder.typicode.com/users")
-    .then((res) => console.log(res.json()))
+    .then((res) => res.json())
+    .then((data) => {
+      let userData = data;
+
+      userData.forEach((user) => {
+        console.log(user);
+
+        userContainer.insertAdjacentHTML(
+          "beforeend",
+          `     
+          <div class="user">
+            <div class="user-profile-wrap">
+                <img class="user-profile" src="content/img/noimg.png" alt="default-image">
+                <div class="user-profile-description">
+                    <h1 class="user-profile-name">${user.name} - ${user.username}<span class="user-age">18</span> </h1>
+                    <h3 class="user-explanations">Pass: ${user.email}</h3>
+                </div>
+            </div>
+            <div class="btn-groups-column">
+                <button class="delete-user-btn">delete</button>
+                <button class="edit-user-btn">edit</button>
+            </div>
+        </div>
+        `
+        );
+      });
+    });
 });
