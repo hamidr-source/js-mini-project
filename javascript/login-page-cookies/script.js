5f87bcc6fac48a1b3b9c39ac12df703f2c9f11dd
hamidr-source <hr454837@gmail.com>
2024-04-02 13:20:27 -0700

    Create Cookie generator

--- javascript/login-page-cookies/script.js
+++ javascript/login-page-cookies/script.js
@@ -1,21 +1,34 @@
+const username = document.querySelector("#username");
+const password = document.querySelector("#password");
+const checkBox = document.querySelector(".ck");
+const loginBtn = document.querySelector("button");
+
 const setCookie = (cookieName, cookieValue, expDay) => {
   const now = new Date();
 
   now.setTime(now.getTime() + expDay * 24 * 60 * 60 * 1000);
 
-  document.cookie = `${cookieName}=${cookieValue};path=/;expires=${now}`
+  document.cookie = `${cookieName}=${cookieValue};path=/;expires=${now}`;
 };
 
 const getCookie = (cookieName) => {
-    const cookiesArray = document.cookie.split(";")
+  const cookiesArray = document.cookie.split(";");
 
-    let mainCookie = null
+  let mainCookie = null;
 
-    cookiesArray.some(cookie => {
-        if (cookie.includes(cookieName)) {
-            mainCookie = cookie.substring(cookie.indexOf("=") + 1)
-            return true
-        }
-    })
-    return mainCookie
-}
+  cookiesArray.some((cookie) => {
+    if (cookie.includes(cookieName)) {
+      mainCookie = cookie.substring(cookie.indexOf("=") + 1);
+      return true;
+    }
+  });
+  return mainCookie;
+};
+
+loginBtn.addEventListener("click", (event) => {
+    event.preventDefault()
+
+    if (checkBox.checked) {
+        setCookie("user-login-token", username.value, 3)
+    }
+})
