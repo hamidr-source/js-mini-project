086de665a9479ceeb93a2cb29c57ee76992c1482
hamidr-source <hr454837@gmail.com>
2024-04-02 13:09:18 -0700

    Add function for get cookies

--- javascript/login-page-cookies/script.js
+++ javascript/login-page-cookies/script.js
@@ -5,3 +5,17 @@ const setCookie = (cookieName, cookieValue, expDay) => {
 
   document.cookie = `${cookieName}=${cookieValue};path=/;expires=${now}`
 };
+
+const getCookie = (cookieName) => {
+    const cookiesArray = document.cookie.split(";")
+
+    let mainCookie = null
+
+    cookiesArray.some(cookie => {
+        if (cookie.includes(cookieName)) {
+            mainCookie = cookie.substring(cookie.indexOf("=") + 1)
+            return true
+        }
+    })
+    return mainCookie
+}
